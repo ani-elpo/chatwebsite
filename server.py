a80a179d9b7bed14c7c39d6fbff8732305789800
ani <anulikapple@gmail.com>
2020-10-14 18:22:42 +0100

    added styling to website and chat

--- server.py
+++ server.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("index.html")
+
+@app.route('/chat')
+def chat():
+    return render_template("chat_page.html", username=request.args.get("username"))
 
 app.run(debug=True)
+
+# http://127.0.0.1:5000/chat
+
