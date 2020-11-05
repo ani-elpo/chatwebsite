d0c0b0d63def4283e83b3ef07d5801102372860f
ani <anulikapple@gmail.com>
2020-11-05 19:32:51 +0000

    if the password is wrong it takes you back to the homepage

--- server.py
+++ server.py
@@ -8,13 +8,20 @@ users={"bob":"bobpw", "jeff":"jeffpw"}
 
 @app.route('/')
 def hello_world():
+    if "password_correct" in session:
+        username = session["username"]
+        password_correct = session["password_correct"]
     return render_template("index.html")
 
 messages=[]
 
 @app.route('/chat', methods=["GET","POST"])
 def chat():
+    if "username" not in session:
+        return redirect("/")
+
     username = session["username"]
+
     if request.method == "POST":
         message=request.form["message"]
         message_info={"author":username, "message":message}
@@ -28,13 +35,23 @@ def login():
 
     if username in users and users[username] == password:
         session["username"] = username
+        password_correct = True
+        session["password_correct"] = password_correct
         return redirect(url_for("chat"))
     else:
+        password_correct = False
+        session["password_correct"] = password_correct
         return redirect("/")
+
+@app.route('/logout')
+def logout():
+    del session["username"]
+    return redirect("/")
 
 
 app.run(debug=True)
 
 # http://127.0.0.1:5000/chat
 
-#homework: get chat fully working (new username thing) sending msgs needs to work again
+#homework: display "password incorrect" message on login page instead of just blank screen
+# find a better way to store passwords?
